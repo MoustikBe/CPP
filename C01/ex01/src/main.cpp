9e163eafc70736004c3e1f49d714db19e2d2fb71
Maxence Isaac Castiau <misaac-c@c2r9s6.42madrid.com>
2025-04-03 12:54:15 +0200

    finish ex01

--- C01/ex01/src/main.cpp
+++ C01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: misaac-c <misaac-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 10:15:55 by misaac-c          #+#    #+#             */
-/*   Updated: 2025/04/01 12:57:38 by misaac-c         ###   ########.fr       */
+/*   Updated: 2025/04/03 12:53:50 by misaac-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int main(int argc, char **argv)
 {
+	int i;
 	Zombie *Zomb;
 
 	if(argc != 3)
@@ -21,7 +22,15 @@ int main(int argc, char **argv)
 		std::cout << "Format expected -> ./Zombie [Nb_Zombie] [Name_Zombie]\n";
 		return(1);
 	}
-	int i = atoi(argv[1]);
+	i = atoi(argv[1]);
+	if (i == 0)
+	{
+		std::cout << "Invalid format for the [Nb_zombie]\n";
+		return(1);
+	} 
 	Zomb = zombieHorde(i, (std::string)argv[2]);
+	for(int j = 0; j < i; j++)
+		Zomb[j].announce();
+	delete[] Zomb;
 	return(0);
 }
--- C01/ex01/src/zombieHorde.cpp
+++ C01/ex01/src/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: misaac-c <misaac-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 12:36:51 by misaac-c          #+#    #+#             */
-/*   Updated: 2025/04/01 13:01:19 by misaac-c         ###   ########.fr       */
+/*   Updated: 2025/04/03 12:50:24 by misaac-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ Zombie* zombieHorde(int N, std::string name)
 {
 	// 1) Creer tous les zombie et les renvois dans un pointeur de la class dans le main
 	Zombie *Zomb = new Zombie[N];
-	for(int i=0; i<=N; i++)
-		Zomb[i].SetName(name); 
+	for(int i=0; i<N; i++)
+		Zomb[i].SetName(name);
 	// Alloue la memoire et leurs donnes leurs nom.
 	return(Zomb);
 }
