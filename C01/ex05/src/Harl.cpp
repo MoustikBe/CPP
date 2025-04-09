ad38650acbfb7a90593a5ea1fe32de6f0cbb8cd6
Maxence Isaac Castiau <misaac-c@c2r6s1.42madrid.com>
2025-04-09 13:10:28 +0200

    finishing C01

--- C01/ex05/src/Harl.cpp
+++ C01/ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: misaac-c <misaac-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 12:53:32 by misaac-c          #+#    #+#             */
-/*   Updated: 2025/04/09 12:42:54 by misaac-c         ###   ########.fr       */
+/*   Updated: 2025/04/09 13:05:39 by misaac-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void Harl::debug()
 {
-    std::cout << "[DEBUG]   -> I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+    std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
 }
 
 void Harl::info()
 {
-    std::cout << "[INFO]    -> I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+    std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 
 void Harl::warning()
 {
-    std::cout << "[WARNING] -> I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+    std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
 }
 
 void Harl::error()
 {
-    std::cout << "[ERROR]   -> This is unacceptable! I want to speak to the manager now.\n";
+    std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now.\n";
 }
 
 void Harl::complain(std::string level)
--- C01/ex06/src/main.cpp
+++ C01/ex06/src/main.cpp
@@ -6,19 +6,47 @@
 /*   By: misaac-c <misaac-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 12:49:29 by misaac-c          #+#    #+#             */
-/*   Updated: 2025/04/09 12:22:08 by misaac-c         ###   ########.fr       */
+/*   Updated: 2025/04/09 13:03:48 by misaac-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main(void)
+int find_val(std::string str)
 {
-	Harl	angry_client;
+	if(str == "DEBUG")
+		return(1);
+	else if(str == "INFO")
+		return(2);
+	else if(str == "WARNING")
+		return(3);
+	else if(str == "ERROR")
+		return(4);
+	return(5);
+}
 
-	angry_client.complain("DEBUG");
-	angry_client.complain("INFO");
-	angry_client.complain("WARNING");
-	angry_client.complain("ERROR");
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+		return(1);
+	Harl	angry_client;
+	int token = find_val(argv[1]);
+
+	switch (token)
+	{
+		case 1:
+			angry_client.complain("DEBUG");
+		case 2:
+			angry_client.complain("INFO");
+		case 3:
+			angry_client.complain("WARNING");
+		case 4:
+			angry_client.complain("ERROR");
+			break;
+	default:
+		std::cout << "[ Probably complaining about insignificant problems ]\n";
+		break;
+	}	
 	return(0);
 }
