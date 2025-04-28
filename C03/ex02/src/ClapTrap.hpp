4fabd27a1efc279843e747d9c4b904aac0774359
Maxence Isaac Castiau <misaac-c@c2r19s6.42madrid.com>
2025-04-28 13:40:39 +0200

    finishing ex02

---
+++ C03/ex02/src/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAP_TRAP_HPP
+#define CLAP_TRAP_HPP
+
+#include <iostream>
+#include <aio.h>
+
+class ClapTrap
+{
+protected:
+    std::string _Name;
+    int      _HitPoints;
+    int      _Stamina;
+    int      _AttackDamage;
+public:
+    ClapTrap();
+    ClapTrap(std::string name);
+    ClapTrap(const ClapTrap &other);
+    ClapTrap& operator=(const ClapTrap &other);
+    ~ClapTrap();
+    void        attack(const std::string& target);
+    void        takeDamage(unsigned int amount);
+    void        beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ C03/ex02/src/main.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misaac-c <misaac-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 11:05:27 by misaac-c          #+#    #+#             */
+/*   Updated: 2025/04/28 13:39:42 by misaac-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+int main()
+{
+	ClapTrap a;
+	ClapTrap b("bot_B");
+	std::cout << "------------\n";
+	a.attack("a tree");
+	b.attack("a rock");
+	a.beRepaired(5);
+	b.beRepaired(10);
+	a.takeDamage(10);
+	b.takeDamage(15);
+	std::cout << "------------\n";
+	
+	ScavTrap c;
+	ScavTrap d("bot_D");
+	std::cout << "------------\n";
+	c.attack("a wall");
+	d.attack("a glass");
+	c.beRepaired(1);
+	d.beRepaired(2);
+	c.takeDamage(1);
+	d.takeDamage(2);
+	std::cout << "------------\n";
+
+	FragTrap e;
+	FragTrap f("bot_F");
+	std::cout << "------------\n";
+	e.attack("a paper");
+	f.attack("a truck");
+	e.beRepaired(1);
+	f.beRepaired(2);
+	e.takeDamage(1);
+	f.takeDamage(2);
+	std::cout << "------------\n";
+	return (0);
+}
