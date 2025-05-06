2c1f1881ee6232d645dcb059f58bba5d68ea3ee1
Maxence Isaac Castiau <misaac-c@c2r7s6.42madrid.com>
2025-05-06 11:38:32 +0200

    pushing C01

--- C01/ex03/src/main.cpp
+++ C01/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: misaac-c <misaac-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:50:04 by misaac-c          #+#    #+#             */
-/*   Updated: 2025/05/06 11:10:03 by misaac-c         ###   ########.fr       */
+/*   Updated: 2025/05/06 11:37:28 by misaac-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,17 @@
 
 int main()
 {
-	{
-		Weapon club;
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("guns");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("apples");
-		HumanB jim("Jim");
-		jim.attack();
-		jim.setWeapon(club);
-		club.setType("swords");
-		jim.attack();
-	}
+	Weapon club;
+	HumanA bob("Bob", club);
+	bob.attack();
+	club.setType("guns");
+	bob.attack();
+	Weapon club = Weapon("apples");
+	HumanB jim("Jim");
+	jim.attack();
+	jim.setWeapon(club);
+	club.setType("swords");
+	jim.attack();
+	
 	return (0);
 }
