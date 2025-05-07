dfc03aa875f1ca83a3a056d2f6b3df83d2183313
Maxence Isaac Castiau <misaac-c@c2r9s6.42madrid.com>
2025-05-07 11:07:11 +0200

    fixing the main

--- C01/ex03/src/main.cpp
+++ C01/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: misaac-c <misaac-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:50:04 by misaac-c          #+#    #+#             */
-/*   Updated: 2025/05/06 11:37:28 by misaac-c         ###   ########.fr       */
+/*   Updated: 2025/05/07 11:06:00 by misaac-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@
 
 int main()
 {
-	Weapon club;
+	Weapon club = Weapon("apples");
 	HumanA bob("Bob", club);
 	bob.attack();
 	club.setType("guns");
 	bob.attack();
-	Weapon club = Weapon("apples");
 	HumanB jim("Jim");
 	jim.attack();
 	jim.setWeapon(club);
