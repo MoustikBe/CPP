589f63d722c40b2d9aa23850197448933b9c15b6
Maxence Isaac Castiau <misaac-c@c2r8s6.42madrid.com>
2025-05-19 13:35:12 +0200

    working on ex02

---
+++ C06/ex02/src/Base.cpp
@@ -0,0 +1,17 @@
+#include "Base.hpp"
+
+Base* generate(void)
+{
+    //Faire une generation aleatoire d'un des trois type A B C et en retourner l'addresse
+}
+
+void identify(Base* p)
+{
+    //Identifier le type A B ou C a partir d'un pointeur 
+}
+
+
+void identify(Base& p)
+{
+    //Identifier le type A B ou C a partir d'une addresse 
+}
