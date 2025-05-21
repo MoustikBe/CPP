705aa973e8bd8c7ab48d37e113a7bb5603bfc6ca
Maxence Isaac Castiau <misaac-c@c2r7s6.42madrid.com>
2025-05-21 12:23:35 +0200

    ex00 C08

--- C08/ex00/header/easyfind.hpp
+++ C08/ex00/header/easyfind.hpp
@@ -1,14 +1,20 @@
 #ifndef EASY_FIND_HPP
 # define EASY_FIND_HPP
 
-#include <iostream>
-#include <vector>
+# include <iostream>
+# include <vector>
+# include <iterator>
+# include <algorithm>
 
 template<typename T>
-typename T::iterator easyfind(T& vec, int nb)
+typename T::const_iterator easyfind(const T& vec, int nb)
 {
-    return std::find(vec.begin(), vec.end(), nb);
-}
+    typename T::const_iterator it;
 
+    it = std::find(vec.begin(), vec.end(), nb);
+    if(it == vec.end())
+        throw(std::out_of_range("out of bound"));
+    return (it);
+}
 
 #endif
--- C08/ex00/src/main.cpp
+++ C08/ex00/src/main.cpp
@@ -1,13 +1,49 @@
 #include "../header/easyfind.hpp"
 
+void DisplayVecContent(const std::vector<int>& VectorTest)
+{
+    for(int i = 0; i < VectorTest.size(); i++)
+    {
+        std::cout << VectorTest[i];
+        std::cout << " ";
+    }
+    std::cout << "\n";
+}
+
+
 int main( void ) 
 {
-    std::vector<int> VectorTest;
-
-    VectorTest.push_back(1);
-    VectorTest.push_back(4);
-    VectorTest.push_back(6220);
-    VectorTest.push_back(4444);
-    std::vector<int>::iterator it = easyfind(VectorTest, 4);
-    std::cout << *it <<  "\n";
+    // All good scenario // 
+    try
+    {
+        std::vector<int> VectorTest; 
+        VectorTest.push_back(1);
+        VectorTest.push_back(2);
+        VectorTest.push_back(5);
+        VectorTest.push_back(745);
+        std::vector<int>::const_iterator it = easyfind(VectorTest, 2);
+        DisplayVecContent(VectorTest);
+        std::cout << "Vector index trouver -> : " << *it <<"\n";
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << " # ------------- # \n";
+    // Out of bound scenario // 
+    try
+    {
+        std::vector<int> VectorTest; 
+        VectorTest.push_back(1);
+        VectorTest.push_back(2);
+        VectorTest.push_back(5);
+        VectorTest.push_back(745);
+        std::vector<int>::const_iterator it = easyfind(VectorTest, 22);
+        DisplayVecContent(VectorTest);
+        std::cout << "Vector index trouver -> : " << *it <<"\n";
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
