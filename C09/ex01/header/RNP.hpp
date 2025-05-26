a0cc5aaf2ba5a14ac11428369a0250061d333be5
MoustikBe <maxenceic2@gmail.com>
2025-05-26 19:16:44 +0200

    almost done, needing test

--- C09/ex01/header/RNP.hpp
+++ C09/ex01/header/RNP.hpp
@@ -4,17 +4,6 @@
 #include <iostream>
 #include <stack>
 #include <algorithm>
-
-class RNP
-{
-private:
-    std::stack<int> _stack;
-public:
-    RNP();
-    RNP(const RNP& other);
-    RNP& operator=(const  RNP& other);
-    ~RNP();
-};
-
+#include <span>
 
 #endif
--- C09/ex01/src/main.cpp
+++ C09/ex01/src/main.cpp
@@ -1,19 +1,50 @@
 #include "../header/RNP.hpp"
 
 
-void  operation_scenario(void)
-{
+void  operation_scenario(std::stack<int>& data, char op, int i)
+{   
+    int result, A, B;
+    if(data.size() < 2)
+        return ;
 
+    A = data.top();
+    data.pop();
+    B = data.top();
+    data.pop();
+    switch (op)
+    {
+        case '-':{result = B - A; break;}
+        case '+':{result = B + A; break;}
+        case '*':{result = B * A; break;}
+        case '/':{result = B / A; break;}
+        default: {std::cout << "Error\n"; break;}
+    }
+    data.push(result);
+}
+
+void print_content(std::stack<int> _data)
+{
+    while(_data.size())
+    {
+        std::cout << "Data -> " << _data.top() << "\n";
+        _data.pop();
+    }
+    return ; 
 }
 
 int main(int argc, char **argv)
 {
-    int j = 0;
+    std::stack<int> data;
+
+    int j = 1;
     for(int i = 0; argv[j][i]; i++)
     {
         if(argv[j][i] == '-' || argv[j][i] == '+' || argv[j][i] == '*' || argv[j][i] == '/')
-            operation_scenario();
-    
+            operation_scenario(data, argv[j][i], i);
+        else if(isdigit(argv[j][i]))
+            data.push(argv[j][i] - '0');
     }
-    return(0);
+    if(data.size() != 1)
+        return(std::cout << "Error\n", 0);
+    return(std::cout << data.top() << "\n", 0);
 }
